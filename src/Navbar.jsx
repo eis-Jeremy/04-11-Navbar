9f26880587172e275d58a10bd306d90787cea2c8
eis-Jeremy <eis08.jerry@gmail.com>
2024-08-26 18:24:13 +0200

    Completed Navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useState, useRef } from 'react'
+import { FaBars } from 'react-icons/fa'
+import { links, social } from './data'
+import logo from './logo.svg'
+
+function Navbar() {
+  const [showLinks, setShowLinks] = useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks)
+  }
+
+  const linkStyles = {
+    height: showLinks ? `${linksRef.current.getBoundingClientRect().height}px` : '0px'
+  }
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        
+          <div className='links-container' ref={linksContainerRef} style={linkStyles}>
+            <ul className="links" ref={linksRef}>
+              {links.map((link) => {
+                return (
+                  <li key={link.id}>
+                    <a href={link.url}>{link.text}</a>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+       
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
